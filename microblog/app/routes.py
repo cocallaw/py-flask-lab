f8e390b7c12b28db2d9a975f4fba4eca25ef9acd
cocallaw <coreylcallaway@gmail.com>
2019-12-24 10:18:42 -0500

    Add basic templates

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'User'}
+    posts = [
+        {
+            'author': {'username': 'Bill'},
+            'body': 'Building with Python'
+        },
+        {
+            'author': {'username': 'Scott'},
+            'body': 'Azure the cloud of the future'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
