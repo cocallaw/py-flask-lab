0b0ce15d1fe3ef5f18e6bb1049ae897f1017695d
cocallaw <coreylcallaway@gmail.com>
2019-12-27 17:13:37 -0500

    DB Init

--- microblog/config.py
+++ microblog/config.py
@@ -2,7 +2,7 @@ import os
 import urllib.parse
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-params = urllib.parse.quote_plus("DRIVER={SQL Server};SERVER=pyclc.database.windows.net;DATABASE=pyflask;UID=FlaskApp01@pyflask;PWD=XXXXXXX")
+params = urllib.parse.quote_plus("DRIVER={SQL Server};SERVER=pyclc.database.windows.net;DATABASE=pyflask;UID=FlaskApp01@sqldb;PWD=P@ssword1!")
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
