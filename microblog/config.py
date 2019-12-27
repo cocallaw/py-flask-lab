3d8b752833038c8a2404cb77c62db4741438f816
cocallaw <coreylcallaway@gmail.com>
2019-12-27 17:07:37 -0500

    Adding DB integration

--- microblog/config.py
+++ microblog/config.py
@@ -1,4 +1,10 @@
 import os
+import urllib.parse
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+params = urllib.parse.quote_plus("DRIVER={SQL Server};SERVER=pyclc.database.windows.net;DATABASE=pyflask;UID=FlaskApp01@pyflask;PWD=XXXXXXX")
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "mssql+pyodbc:///?odbc_connect=%s" % params
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
